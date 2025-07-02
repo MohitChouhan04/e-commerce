120c06809383cc516013821f346676937fb494d1
MohitChouhan04 <m3301040@gmail.com>
2025-07-02 14:44:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import logo from './logo.svg';
+import react from 'react';
+import {BrowserRouter, Routes ,Route} from "react-router-dom"
 import './App.css';
-
+import Home from './Component/Home';
+import About from './Component/About';
+import Products from './Component/Products';
+import Contact from './Component/Contact';
+import SingleProduct from './Component/SingleProduct';
+import Cart from './Component/Cart';
+import ErrorPage from './Component/ErrorPage';
 function App() {
   return (
-    <div>hello jee</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/singleproduct/:id" element={<SingleProduct/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+
+        
+
+      </Routes>
+    </BrowserRouter>
 
   );
 }
